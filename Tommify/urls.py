85f9a48abd542b3c5efd837706ea00e56b7eadc6
paleotommytechy <tomiwadesignandtech00@gmail.com>
2025-04-17 05:51:17 +0100

    battling with default profile image displaying in production.

--- Tommify/urls.py
+++ Tommify/urls.py
@@ -28,5 +28,8 @@ if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
+    from django.views.static import serve
+    from django.urls import re_path
+    
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [ re_path(r'"media/(?P<page>,*)$', serve, {'document_root': settings.MEDIA_ROOT}), ]
